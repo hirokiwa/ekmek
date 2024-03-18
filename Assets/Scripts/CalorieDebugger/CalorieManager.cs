6bdcad4df562dd82c9f3ab727917c80e56a08516
hirokiwa <hirokifsys@gmail.com>
2024-03-18 18:18:57 +0900

    Cout up calorie base score

--- Assets/Scripts/CalorieDebugger/CalorieManager.cs
+++ Assets/Scripts/CalorieDebugger/CalorieManager.cs
@@ -3,26 +3,39 @@ using UnityEngine.UI;
 
 public class CalorieManager : MonoBehaviour
 {
+  private const float ACCELERATION_THRESHOLD = 2;
+
   public Text JoyConScoreNumber;
 
-  private int JconScore;
+  private float Acceleration;
+  private float JconScore;
 
   void Update()
   {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      HandleDebug();
-    }
+    Debug.Log(AccelerationFilter.instance.last_accel_value);
+
+    setAcceleration(AccelerationFilter.instance?.last_accel_value ?? 0);
+    CountUpJoyConScore();
   }
 
-  void HandleDebug()
+  void CountUpJoyConScore()
   {
-    Debug.Log("Space key pressed down!");
-    setJsonScore(JconScore + 1);
-    setJoyConScoreNumber(JconScore.ToString());
+    float additionalScore = Acceleration > ACCELERATION_THRESHOLD ? Acceleration : 0;
+    updateScore(JconScore + additionalScore);
   }
 
-  private void setJsonScore(int input)
+  private void updateScore(float input)
+  {
+    setJsonScore(input);
+    setJoyConScoreNumber(input.ToString());
+  }
+
+  private void setAcceleration(float input)
+  {
+    Acceleration = input;
+  }
+
+  private void setJsonScore(float input)
   {
     JconScore = input;
   }
